d59d8b31c6ebc76c9d6ccdb2b63bdbfea41c8d4b
Avoid <116073567+Warghto@users.noreply.github.com>
2023-06-14 07:57:09 +0000

    Update 6 files

--- src/index.js
+++ src/index.js
@@ -1,51 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createStore } from "redux";
+import reducer from "./reducer";
+import { inc, dec, rnd } from "./actions";
 
-import App from "./App";
+// import App from "./App";
 
-const initialState = { value: 0 };
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "INC":
-      return {
-        ...state,
-        value: state.value + 1,
-      };
-    case "DEC":
-      return {
-        ...state,
-        value: state.value - 1,
-      };
-    case "RND":
-      return {
-        ...state,
-        value: state.value * action.payload,
-      };
-    default:
-      return state;
-  }
-};
 const store = createStore(reducer);
+const { dispatch, subscribe, getState } = store;
 
 const update = () => {
-  document.getElementById("counter").textContent = store.getState().value;
+  document.getElementById("counter").textContent = getState().value;
 };
 
-store.subscribe(update);
-const inc = () => ({ type: "INC" });
-const dec = () => ({ type: "DEC" });
-const rnd = () => ({ type: "RND", payload: value });
+subscribe(update);
+
+const incDispatch = () => dispatch(inc());
+const decDispatch = () => dispatch(dec());
+const rndDispatch = (value) => dispatch(rnd(value));
 
 document.getElementById("inc").addEventListener("click", () => {
-  store.dispatch(inc());
+  incDispatch();
 });
 document.getElementById("dec").addEventListener("click", () => {
-  store.dispatch(dec());
+  decDispatch();
 });
 document.getElementById("rnd").addEventListener("click", () => {
   const value = Math.floor(Math.random() * 5);
-  store.dispatch(rnd(value));
+  rndDispatch(value);
 });
 
 const rootElement = document.getElementById("root");
