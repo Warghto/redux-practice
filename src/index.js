a08591dc230ac61fc2ba0ba3dc350b4a09f08457
Avoid <116073567+Warghto@users.noreply.github.com>
2023-06-14 13:57:20 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createStore } from "redux";
 import reducer from "./reducer";
@@ -7,29 +7,25 @@ import { inc, dec, rnd } from "./actions";
 const store = createStore(reducer);
 const { dispatch, subscribe, getState } = store;
 
-
-
 const update = () => {
   document.getElementById("counter").textContent = getState().value;
 };
 
 subscribe(update);
 
+const bindActionCreator = () => {};
 
 const incDispatch = () => dispatch(inc());
 const decDispatch = () => dispatch(dec());
 const rndDispatch = (value) => dispatch(rnd(value));
 
-document.getElementById("inc").addEventListener("click", () => {
-  dispatch(inc());
-});
-document.getElementById("dec").addEventListener("click", () => {
-  dispatch(dec());
-});
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 5);
-  dispatch(rnd(value));
+document.getElementById("inc").addEventListener("click", incDispatch);
 
+document.getElementById("dec").addEventListener("click", decDispatch);
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+  rndDispatch(value);
 });
 
 const rootElement = document.getElementById("root");
